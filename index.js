ccadd1addb4141540670d190a696e2ae91882505
Ahmed Shalash <ah.shalash@gmail.com>
2024-02-06 00:22:41 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -11,6 +11,34 @@ const tutorials = [
   'what is JSONP?'
 ];
 
-const titleCased = () => {
-  return tutorials
+// Function to capitalize the first letter of a word
+function capitalizeFirstLetter(word) {
+  return word.charAt(0).toUpperCase() + word.slice(1);
 }
+
+// Function to convert a string to title case
+function toTitleCase(str) {
+  // Split the string into words
+  const words = str.split(' ');
+  
+  // Capitalize the first letter of each word
+  const capitalizedWords = words.map(word => capitalizeFirstLetter(word));
+  
+  // Join the words back together to form the title in title case
+  return capitalizedWords.join(' ');
+}
+
+// Use map() to convert each tutorial title to title case
+const titlesInTitleCase = tutorials.map(tutorial => toTitleCase(tutorial));
+
+// Print the new array of titles in title case
+console.log(titlesInTitleCase);
+
+// Function to return titles in title case
+const titleCased = () => {
+  return titlesInTitleCase;
+}
+
+module.exports = {
+  titleCased
+};
